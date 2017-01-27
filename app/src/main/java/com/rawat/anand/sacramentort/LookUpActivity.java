94682d7cbf1079107a1a5f0bc3930c1639707938
anandrawat90 <anandrawat90@gmail.com>
2017-01-27 13:20:25 -0800

    minor changes to look up view layout

--- app/src/main/java/com/rawat/anand/sacramentort/LookUpActivity.java
+++ app/src/main/java/com/rawat/anand/sacramentort/LookUpActivity.java
@@ -3,8 +3,6 @@ package com.rawat.anand.sacramentort;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -36,14 +34,6 @@ public class LookUpActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 stop = DBUtility.stops.get(position);
                 openEditActivity(busStopList.getContext());
-            }
-        });
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
